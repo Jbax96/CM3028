70e3ddabe6ecbd71c2c50f10c6759b2954aa9b3d
daviesa306 <daviesa306@outlook.com>
2016-12-05 15:17:16 +0000

    Add files via upload

--- signup.php
+++ signup.php
@@ -5,6 +5,9 @@
  * Date: 30/11/2016
  * Time: 13:01
  */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 include ("Dbconnect.php");
 $name = $_POST["name"];
@@ -16,9 +19,9 @@ $sql = "INSERT INTO USERS ( userID, password, emailAddress, accessID) VALUES ( '
 
 
 if (mysqli_query($db, $sql)) {
-  echo "New record created successfully";
+    echo "New record created successfully";
 } else {
-  echo mysqli_error($db);
+    echo "Error: " .$sql. mysqli_error($db);
 }
 
 mysqli_close($db);
