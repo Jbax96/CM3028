2aee1e2379b887625ac81c1c4d89e27e0a7eac6a
Jbax96 <1401407@rgu.ac.uk>
2016-12-05 14:21:13 +0000

    Add files via upload

--- Dbconnect.php
+++ Dbconnect.php
@@ -11,16 +11,110 @@ define('DB_USERNAME','bc1b3a7172a2b2');
 define('DB_PASSWORD','e18f37e0');
 define('DB_DATABASE','goportlethen');
 
-$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$db = mysqli_connect(DB_SERVER,DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 if($db ->connect_errno){
-    die('Connection failed: '.$db->connect_error.'.');
+    die('ConnectFailed['.$db->connect_error.'}');
 }
 
-echo "Connected successfully.<br>";
+echo "Connected successfully";
 
-/* Moved the creation of tables to createtables.php */
+
+//SQL To Create Table USERS
+$sql = "CREATE TABLE 'USERS' (
+UserID VARCHAR(15) NOT NULL,
+Password VARCHAR(30) NOT NULL,
+emailAddress VARCHAR(50) NOT NULL,
+AccessID VARCHAR(6),
+PRIMARY KEY('UserID')
+)";
+
+//SQL To Create Table ACCESS RIGHTS
+$sql = "CREATE TABLE 'ACCESSRIGHTS'(
+AccessID CHAR(6) NOT NULL,
+UserID VARCHAR(15) NOT NULL,
+description VARCHAR(60) NOT NULL,
+PRIMARY KEY('AccessID'),
+CONSTRAINT LOCATION_ibfk_1 FOREIGN KEY (UserID) REFERENCES USERS (UserID)
+)";
+
+//SQL To Create Table CLUB
+$sql = "CREATE TABLE 'CLUB'(
+ClubID CHAR(6) NOT NULL,
+AdminID VARCHAR(15) NOT NULL,
+clubName VARCHAR(30) NOT NULL,
+clubDescription VARCHAR(50) NOT NULL,
+contactInfo VARCHAR(20) NOT NULL,
+Genre VARCHAR(50) NOT NULL,
+PRIMARY KEY ('ClubID'),
+CONSTRAINT LOCATION_ibfk_2 FOREiGN KEY (AdminID) REFERENCES USERS (UserID)
+)";
+
+//SQL To Create Table PHOTO
+$sql = "CREATE TABLE 'PHOTO'(
+PhotoID CHAR(6)NOT NULL,
+ClubID CHAR(6) NOT NULL,
+AdminID VARCHAR(15) NOT NULL,
+FOREIGN KEY fk_admin(AdminID)
+   REFERENCES USERS(UserID)
+   ON UPDATE CASCADE
+   ON DELETE RESTRICT,
+photo LONGBLOB NOT NULL,
+description VARCHAR(50) NOT NULL,
+PRIMARY KEY ('PhotoID'),
+CONSTRAINT LOCATION_ibfk_3  FOREIGN KEY (ClubID) REFERENCES LOCATIONS (LocationID),
+CONSTRAINT LOCATION_ibfk_4 FOREIGN KEY (AdminID) REFERENCES USERS(UserID)
+)";
+
+//SQL To Create Table GENRE
+$sql = "CREATE TABLE 'GENRE'(
+Genre VARCHAR(20) NOT NULL ,
+ClubID CHAR(6) NOT NULL,
+name VARCHAR(50) NOT NULL,
+AdminID VARCHAR(15) NOT NULL,
+name VARCHAR(50) NOT NULL,
+description VARCHAR(50) NOT NULL,
+PRIMARY KEY ('Genre'),
+CONSTRAINT LOCATION_ibfk_5 FOREIGN KEY (LocationID) REFERENCES LOCATIONS (LocationID),
+CONSTRAINT LOCATION_ibfk_6  FOREIGN KEY (UserID) REFERENCES USERS (USerID)
+)";
+
+//SQL To Create Table LOCATIONS
+
+$sql ="CREATE TABLE 'LOCATIONS'(
+LocationID CHAR(6) NOT NULL,
+name VARCHAR(30) NOT NULL,
+startPoint VARCHAR(10) NOT NULL,
+endPoint VARCHAR(10) NOT NULL,
+locationType() VARCHAR(20) NOT NULL ,
+PRIMARY KEY('LocationID')
+)
+ALTER TABLE<LOCATIONS> ADD CONSTRAINT 
+my_constraint CHECK (locationType = 'Viewpoint' OR locationType = 'Routes' OR locationType = 'Landmarks')
+";
+
+//SQL To Create Table AREA
+$sql ="CREATE TABLE 'AREA'(
+AreaID CHAR(6) NOT NULL ,
+LocationID CHAR(6) NOT NULL,
+name VARCHAR(50) NOT NULL,
+description VARCHAR(50) NOT NULL,
+PRIMARY KEY ('AreaID'),
+KEY LocationID (LocationID),
+CONSTRAINT LOCATION_ibfk_7 FOREIGN KEY (LocationID) REFERENCES LOCATIONS (LocationID)
+)";
+
+
+$sql = "CREATE TABLE 'GEODATA'(
+GeoID CHAR(6) NOT NULL,
+LocationID CHAR(6) NOT NULL,
+description VARCHAR(50) NOT NULL,
+PRIMARY KEY ('GeoID'),
+CONSTRAINT LOCATION_ibfk_8 FOREIGN KEY (LocationID) REFERENCES LOCATIONS (LocationID)
+)";
 
 ?>
+mysqli_close($db);
+)"
 
 
 
