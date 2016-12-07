402306eac5cb6f064190b493e49c09b7121af5f7
eldhrin <zanaris.valu@hotmail.co.uk>
2016-12-07 11:59:00 +0000

    gg scrubs

---
+++ checkclubadmin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1402677
+* Date: 05/12/2016
+* Time: 16:31
+*/
+/*-- IF USERID == ADMINID then can edit*/
+include ("Dbconnect.php");
+$USERID = $ADMINID["name"];
+
+}
+
+?>
