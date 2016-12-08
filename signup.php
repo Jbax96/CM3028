b9f51fce5125cca41ccd339d543bb8d5bd129c99
daviesa306 <daviesa306@outlook.com>
2016-12-08 12:17:10 +0000

    Add files via upload

--- signup.php
+++ signup.php
@@ -15,6 +15,7 @@ $query = mysql_query("SELECT UserID FROM USER WHERE UserID=$name", $con);
 
 if(mysql_num_rows($query) > 0) {
     $_SESSION['name']=$name;
+    header("Location: singup.php");
     echo("Username already exist");
 } else{
     $sql = "INSERT INTO USERS ( userID, password, emailAddress, accessID) VALUES ( '$name', '$password', '$email', '$accountType')";
