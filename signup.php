780ef5fd9e8abc3777ff5b427cb876118171a5b6
Jbax96 <1401407@rgu.ac.uk>
2016-12-08 11:13:12 +0000

    Add files via upload

--- signup.php
+++ signup.php
@@ -8,23 +8,25 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
 include ("Dbconnect.php");
 $name = $_POST["name"];
 $password = $_POST["password"];
 $email = $_POST["email"];
 $accountType = $_POST["accountType"];
+$query = mysql_query("SELECT UserID FROM USERS WHERE UserID=$name", $con);
 
-$sql = "INSERT INTO USERS ( userID, password, emailAddress, accessID) VALUES ( '$name', '$password', '$email', '$accountType')";
-
-
+if (mysql_num_rows($query) != 0)
+{
+    echo "Username already exists";
+}
+    else {
+        $sql = "INSERT INTO USERS ( userID, password, emailAddress, accessID) VALUES ( '$name', '$password', '$email', '$accountType')";
+}
 if (mysqli_query($db, $sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " .$sql. mysqli_error($db);
 }
-
 header("Location: index.html");
-
 mysqli_close($db);
 ?>
