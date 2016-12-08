7b3f767f1e8a88da4c02974df0cc274f2af4d54a
daviesa306 <daviesa306@outlook.com>
2016-12-08 11:56:58 +0000

    Add files via upload

--- signup.php
+++ signup.php
@@ -5,30 +5,27 @@
  * Date: 30/11/2016
  * Time: 13:01
  */
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 include ("Dbconnect.php");
 $name = $_POST["name"];
 $password = $_POST["password"];
 $email = $_POST["email"];
 $accountType = $_POST["accountType"];
-$query = mysql_query("SELECT UserID FROM USERS WHERE UserID=$name", $con);
 
-if (mysql_num_rows($query) != 0)
-{
-    echo "Username already exists";
-}
-    else {
-        $sql = "INSERT INTO USERS ( userID, password, emailAddress, accessID) VALUES ( '$name', '$password', '$email', '$accountType')";
+if(mysql_num_rows($query) != 0) {
+    echo("Username already exist");
+} else{
+    $sql = "INSERT INTO USERS ( userID, password, emailAddress, accessID) VALUES ( '$name', '$password', '$email', '$accountType')";
 
-        if (mysqli_query($db, $sql)) {
-            echo "New record created successfully";
-            header("Location: index.html");
-        } else {
-            echo "Error: " . $sql . mysqli_error($db);
-        }
+    if (mysqli_query($db, $sql)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " .$sql. mysqli_error($db);
     }
+}
+
+
+
+header("Location: index.html");
 
 mysqli_close($db);
 ?>
