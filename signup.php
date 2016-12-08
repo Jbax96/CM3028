715fda687709c9c4eddb3410eaaef093b82ee491
daviesa306 <daviesa306@outlook.com>
2016-12-08 12:15:14 +0000

    Add files via upload

--- signup.php
+++ signup.php
@@ -11,8 +11,10 @@ $name = $_POST["name"];
 $password = $_POST["password"];
 $email = $_POST["email"];
 $accountType = $_POST["accountType"];
+$query = mysql_query("SELECT UserID FROM USER WHERE UserID=$name", $con);
 
-if(mysql_num_rows($query) != 0) {
+if(mysql_num_rows($query) > 0) {
+    $_SESSION['name']=$name;
     echo("Username already exist");
 } else{
     $sql = "INSERT INTO USERS ( userID, password, emailAddress, accessID) VALUES ( '$name', '$password', '$email', '$accountType')";
@@ -20,7 +22,7 @@ if(mysql_num_rows($query) != 0) {
     if (mysqli_query($db, $sql)) {
         echo "New record created successfully";
     } else {
-        echo "Username invalid";
+        echo "Error: " .$sql . mysqli_error($db);
     }
 }
 
