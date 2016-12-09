4b1e52a400e1dd9c5f7108896172a5faabae324a
Jbax96 <1401407@rgu.ac.uk>
2016-12-09 17:06:25 +0000

    Update addclub.php

--- addclub.php
+++ addclub.php
@@ -22,7 +22,7 @@ if( mysql_num_rows($query) != 0){
 }
     else {
 
-        $sql = "INSERT INTO CLUB (ClubID, AdminID, clubName, clubDescription, contactInfo, Genre) VALUES ('$clubID' , '$adminID' , '$clubName' , '$clubDescription' , '$contactDetails' , '$Genre')";
+        $sql = "INSERT INTO CLUB (ClubID, AdminID, clubName, clubDescription, contactInfo, Genre) VALUES ('$clubID' , '$adminID' , '$clubName' , '$clubDescription' , '$contactDetails' , '$clubGenre')";
         if (mysqli_query($db, $sql)) {
             echo "New club created successfully";
             header("Location: index.html");
