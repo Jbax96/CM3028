3ea0d8dbdf78a07b455ea89e97917ee60e3ab285
Jbax96 <1401407@rgu.ac.uk>
2016-12-10 22:10:06 +0000

    Add files via upload

--- resetPassword.php
+++ resetPassword.php
@@ -1,15 +1,12 @@
 <?php
 include ("Dbconnect.php");
-$name = $_POST["name"];
-$password = $_POST["password"];
-$email = $_POST["email"];
-$accountType = $_POST["accountType"];
+$name = $_POST["resetUser"];
 $newPassword = $_POST["newPassword"];
 
 
 $sql = "UPDATE USERS
 SET password='$newPassword';
-WHERE userID='$name''";
+WHERE userID='$name'";
 
 mysqli_close($db);
 ?>
