2778cb3a5fdd806ee02b934387bb8b97de2ce1bd
Jbax96 <1401407@rgu.ac.uk>
2016-12-10 22:18:19 +0000

    Update resetPassword.php

--- resetPassword.php
+++ resetPassword.php
@@ -4,12 +4,7 @@ $name = $_POST["resetUser"];
 $newPassword = $_POST["newPassword"];
 
 
-$sql = "UPDATE `users` SET
-`UserID` = 'Alfred Hendry',
-`Password` = 'password',
-`emailAddress` = 'alfredhendry@talktalk.net',
-`AccessID` = '3'
-WHERE `UserID` = 'Alfred Hendry' AND `UserID` = 'Alfred Hendry' COLLATE utf8mb4_bin";
+$sql = "UPDATE `users` SET password = '$newPassword' WHERE `UserID` = '$name'";
 
 mysqli_close($db);
 ?>
