156e3563010c55a9ad83ad84657ffacd0585b692
Jbax96 <1401407@rgu.ac.uk>
2016-12-12 18:22:25 +0000

    Add files via upload

--- XML.php
+++ XML.php
@@ -9,15 +9,15 @@ $result =$db-> query($query);
 if(!result){
     die('Nothing in result');
 }
-
 header("Content-type: text/xml");
 
 while($row = $result -> fetch_array()) {
     $node = $dom->createElement("location");
     $newnode = $parnode->appendChild($node);
+    $newnode->setAttribute("LocationID", $row['Location ID']);
     $newnode->setAttribute("name", $row['Name']);
-    $newnode->setAttribute("Longitude", $row['Longitude']);
     $newnode->setAttribute("Latitude", $row['Latitude']);
+    $newnode->setAttribute("Longitude", $row['Longitude']);
     $newNode->setAttribute("locationType", $row['Location Type']);
 }
 
