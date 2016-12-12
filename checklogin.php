627d894394ad168b295b66454434f428b5b5e932
Jbax96 <1401407@rgu.ac.uk>
2016-12-12 23:55:36 +0000

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -4,7 +4,7 @@ $name = $_POST["name"];
 $password = $_POST["password"];
 $nameQuery = mysql_query("SELECT UserID FROM USERS WHERE UserID=$name", $con);
 
-if (mysql_num_rows($query) != 0)
+if (mysql_num_rows($namequery) != 0)
 {
     $passQuery = mysql_query("SELECT Password FROM USERS WHERE Password=$password", $con);
     if ( mysql_num_rows($passQuery) != 0){
