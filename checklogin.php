295f1850a25e1773a7edb82d04d868bfb57d1b34
1408721 <trogdorrunner@gmail.com>
2016-12-12 15:58:52 +0000

    login

--- checklogin.php
+++ checklogin.php
@@ -7,7 +7,7 @@ include ("Dbconnect.php");
 $name = $_POST["name"];
 $password = $_POST["password"];
 
-$nameQuery = mysqli_query($db,"SELECT UserID FROM USERS WHERE UserID='".$name."'");
+$nameQuery = mysqli_query($db,"SELECT UserID FROM USERS WHERE UserID='$name'");
 $passQuery = mysqli_query($db,"SELECT UserID, Password FROM USERS WHERE UserID='$name' AND Password='$password'");
 
 if (mysqli_num_rows($nameQuery) != 0)
