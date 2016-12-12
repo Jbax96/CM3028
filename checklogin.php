dff0e3c27bc94e598d8cfa5b395a3d0bb4c92bcf
Jbax96 <1401407@rgu.ac.uk>
2016-12-12 23:50:38 +0000

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -1,13 +1,7 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 include ("Dbconnect.php");
 $name = $_POST["name"];
 $password = $_POST["password"];
-$email = $_POST["email"];
-$accountType = $_POST["accountType"];
 $nameQuery = mysql_query("SELECT UserID FROM USERS WHERE UserID=$name", $con);
 
 if (mysql_num_rows($query) != 0)
