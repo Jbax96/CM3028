ca6d0a0f13b3e5e2dfb65908b63a1806f54b18fa
1408721 <trogdorrunner@gmail.com>
2016-12-12 10:59:41 +0000

    login and signup

--- signup.php
+++ signup.php
@@ -12,7 +12,7 @@ $password = $_POST["password"];
 $password2 = $_POST["password2"];
 $email = $_POST["email"];
 $accountType = $_POST["accountType"];
-$result = mysqli_query("SELECT UserID FROM USER WHERE UserID=$name");
+$result = mysqli_query("SELECT UserID FROM USERS WHERE UserID=$name");
 
 if(mysql_num_rows($result) != 0) {
     $_SESSION['name']=$name;
