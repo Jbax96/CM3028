7b3a815db7e6cf76d52aac64525a59008ff452a6
daviesa306 <daviesa306@outlook.com>
2016-12-12 18:04:25 +0000

    Add files via upload

--- allClubs.php
+++ allClubs.php
@@ -2,26 +2,27 @@
 
 include ("Dbconnect.php");
 
-//$query = mysqli_query("SELECT clubName, clubDescription, contactInfo, Genre, AdminID FROM CLUB WHERE clubName={$clubName}, clubDescription={$clubDescription}, contactInfo={$contactInfo}, Genre={$Genre}, AdminID={$AdminID}", $con);
-$query=mysqli_query("SELECT * FROM CLUB");
+$query = mysqli_query("SELECT clubName, clubDescription, contactInfo, Genre, AdminID FROM CLUB WHERE clubName={$clubName}, clubDescription={$clubDescription}, contactInfo={$contactInfo}, Genre={$Genre}, AdminID={$AdminID}", $con);
+//$query=mysqli_query("SELECT * FROM CLUB");
 
 $result =$db-> query($query);
 
 if(!result){
     die('Nothing in result');
 }
+
 while($row = $result -> fetch_array()) {
    // echo(
-        $clubName = $row['clubName'];
-        $clubDescription = $row['clubDescription'];
-        $contactInfo = $row['contactInfo'];
-        $Genre = $row['Genre'];
-        $AdminID = $row['AdminID'];
-     echo "Admin: " . $AdminID . "<br>Club Name: " . $clubName . "<br>Description: " . $clubDescription . "<br>Contact: " . $contactInfo . "<br>Genre: " . $Genre;
+    $AdminID = $row['AdminID'];
+    $clubName = $row['clubName'];
+    $clubDescription = $row['clubDescription'];
+    $contactInfo = $row['contactInfo'];
+    $Genre = $row['Genre'];
+
+    echo "Admin: " .AdminID . "<br>Club Name: " .clubName . "<br>Description: " .$clubDescription . "Contact: " .$contactInfo . "Genre: " .$Genre . "fuck this fuckinng course";
    // );
 }
 
 $result->close();
 $db->close();
-
 ?>
