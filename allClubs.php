57d13974b0116576f400e40b42732f0f4456f4ae
daviesa306 <daviesa306@outlook.com>
2016-12-12 17:28:11 +0000

    Add files via upload

--- allClubs.php
+++ allClubs.php
@@ -1,8 +1,9 @@
 <?php
-//display all clubs
+
 include ("Dbconnect.php");
 
-$query = mysqli_query("SELECT clubName, clubDescription, contactInfo, Genre, AdminID FROM CLUB WHERE clubName={$clubName}, clubDescription={$clubDescription}, contactInfo={$contactInfo}, Genre={$Genre}, AdminID={$AdminID}", $con);
+//$query = mysqli_query("SELECT clubName, clubDescription, contactInfo, Genre, AdminID FROM CLUB WHERE clubName={$clubName}, clubDescription={$clubDescription}, contactInfo={$contactInfo}, Genre={$Genre}, AdminID={$AdminID}", $con);
+$query=mysqli_query("SELECT * FROM CLUB");
 
 $result =$db-> query($query);
 
@@ -10,7 +11,14 @@ if(!result){
     die('Nothing in result');
 }
 while($row = $result -> fetch_array()) {
-    echo($query);
+   // echo(
+        $clubName = $row['clubName'];
+        $clubDescription = $row['clubDescription'];
+        $contactInfo = $row['contactInfo'];
+        $Genre = $row['Genre'];
+        $AdminID = $row['AdminID'];
+     echo "Admin: " .AdminID . "<br>Club Name: " .clubName . "<br>Description: " .$clubDescription . "Contact: " .$contactInfo . "Genre: " .$Genre;
+   // );
 }
 
 $result->close();
