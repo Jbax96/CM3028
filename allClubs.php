8ed2900fd9407ca71c7864146bf3d5f11430c149
daviesa306 <daviesa306@outlook.com>
2016-12-12 19:56:12 +0000

    Add files via upload

--- allClubs.php
+++ allClubs.php
@@ -27,7 +27,35 @@
         <!-- /.row -->
 
         <hr>
+        </head>
+        <?php
 
+        include ("Dbconnect.php");
+
+        //$query = mysqli_query("SELECT clubName, clubDescription, contactInfo, Genre, AdminID FROM CLUB WHERE clubName={$clubName}, clubDescription={$clubDescription}, contactInfo={$contactInfo}, Genre={$Genre}, AdminID={$AdminID}", $con);
+        $query="SELECT * FROM CLUB";
+
+        $result =$db-> query($query);
+
+        if(!result){
+            die('Nothing in result');
+        }
+        echo "<h1> List of Clubs</h1>";
+        while($row = $result -> fetch_array()) {
+            $AdminID = $row['AdminID'];
+            $clubName = $row['clubName'];
+            $clubDescription = $row['clubDescription'];
+            $contactInfo = $row['contactInfo'];
+            $Genre = $row['Genre'];
+
+            echo "<p>" . $AdminID . " " . $clubName . " " . $clubDescription . " " . $contactInfo . " " . $Genre . " ";
+        }
+
+        $result->close();
+        $db->close();
+        ?>
+
+        <head>
         <!-- Pagination -->
         <div class="row text-center">
             <div class="col-lg-12">
@@ -127,30 +155,3 @@
         <!-- /.container -->
     </nav>
 </head>
-
-<?php
-
-include ("Dbconnect.php");
-
-//$query = mysqli_query("SELECT clubName, clubDescription, contactInfo, Genre, AdminID FROM CLUB WHERE clubName={$clubName}, clubDescription={$clubDescription}, contactInfo={$contactInfo}, Genre={$Genre}, AdminID={$AdminID}", $con);
-$query="SELECT * FROM CLUB";
-
-$result =$db-> query($query);
-
-if(!result){
-    die('Nothing in result');
-}
-echo "<h1> List of Clubs</h1>";
-while($row = $result -> fetch_array()) {
-    $AdminID = $row['AdminID'];
-    $clubName = $row['clubName'];
-    $clubDescription = $row['clubDescription'];
-    $contactInfo = $row['contactInfo'];
-    $Genre = $row['Genre'];
-
-    echo "<p>" . $AdminID . " " . $clubName . " " . $clubDescription . " " . $contactInfo . " " . $Genre . " ";
-}
-
-$result->close();
-$db->close();
-?>
