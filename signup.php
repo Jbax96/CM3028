1beed979eff527a113e3b222542e6553d2fc6b55
1408721 <trogdorrunner@gmail.com>
2016-12-12 11:02:32 +0000

    login and signup

--- signup.php
+++ signup.php
@@ -17,7 +17,7 @@ $result = mysqli_query("SELECT UserID FROM USERS WHERE UserID=$name");
 if(mysql_num_rows($result) != 0) {
     $_SESSION['name']=$name;
     echo("Username already exists");
-} else if ($password = $password2){
+} else if ($password == $password2){
     $sql = "INSERT INTO USERS ( UserID, Password, emailAddress, accessID) VALUES ( '$name', '$password', '$email', '$accountType')";
 
     if (mysqli_query($db, $sql)) {
